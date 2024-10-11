b213dc801ecb244e53b65bf8b257238a922f518a
mac <0924>
2024-10-11 23:25:39 +0800

    intro

---
+++ src/main/java/com/kerwim/frameworklearnelasticsearch/FrameworkLearnElasticsearchApplication.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnelasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkLearnElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkLearnElasticsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kerwim/frameworklearnelasticsearch/FrameworkLearnElasticsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerwim.frameworklearnelasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkLearnElasticsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
